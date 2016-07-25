8a6aa52556c50aef393c73f8b0d17fcc60a26a04
Esteban Galvis <tebandesade@gmail.com>
2016-07-25 12:18:05 -0500

    no message

---
+++ Assets/Arreglo.cs
@@ -0,0 +1,221 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Arreglo : MonoBehaviour {
+
+
+	public GameObject[] objetos ;
+	private GameObject primerObjeto; 
+	public  Button botonAgregar;
+	public Button botonBorrar;
+	public InputField campoInput;
+	private Color porDefecto; 
+	public Button inicio;
+	Transform padre;
+	public int num333 = 1;
+
+	int n =2;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+
+		primerObjeto = objetos [0];
+		padre = primerObjeto.transform.parent;
+		primerObjeto.SetActive (false);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Debug.Log(campoInput.text + "q" );
+		//Debug.Log();
+	 //	deleteFromArray();
+	}
+
+public	void addToArray()
+	{
+
+		if(verificarCampoInput())
+		{
+			int numero = int.Parse(campoInput.textComponent.text);
+			objetos [numero].GetComponent<MeshRenderer> ().material.color = Color.magenta;
+		}
+	
+		else
+		{
+			for (int i = 0; i < objetos.Length; i++) 
+			{
+				//Debug.Log ("Entro a bucle");
+				bool estaVacia = revisarColor (objetos [i].GetComponent<MeshRenderer> ().material.color);
+					
+				if (estaVacia) 
+				{
+					//Debug.Log ("Entro a estaVacia");
+					objetos [i].GetComponent<MeshRenderer> ().material.color = Color.magenta;
+					//	Debug.Log(objetos [i].GetComponent<MeshRenderer> ().material.mainTexture);
+					break;
+				}
+				if (objetos [objetos.Length-1].GetComponent<MeshRenderer>().material.color ==Color.magenta ) {
+					reArrangeArray();
+					break;
+				}
+			}
+			//Check 
+		}
+
+
+
+	}
+
+	public bool revisarColor(Color actual)
+	{
+		if (actual == porDefecto) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public bool verificarCampoInput()
+	{
+		//Debug.Log ("Entro a verificar input " + campoInput.textComponent.text);
+		if (campoInput.textComponent.text.Length > 0) 
+		{
+			//Debug.Log ("Entro a validar input");
+			return true;
+		}
+		return false;
+	}
+
+	public void activarBotones()
+	{
+		crearCasillas ();
+		botonAgregar.gameObject.SetActive (true);
+		botonBorrar.gameObject.SetActive (true);
+		campoInput.gameObject.SetActive (true);
+
+		inicio.gameObject.SetActive (false);
+	}
+
+	void crearCasillas()
+	{
+		primerObjeto.SetActive (true);
+		float x = primerObjeto.transform.position.x;
+
+
+		for (int i = 1; i < objetos.Length; i++) 
+		{
+
+			objetos [i] = Instantiate (primerObjeto);
+			objetos [i].transform.parent = padre.transform;
+
+			x++;
+			x = x + 0.5f;
+			objetos [i].transform.position = new Vector3 (0.5f,0,x);
+
+		}
+		porDefecto = objetos [0].GetComponent<MeshRenderer> ().material.color;
+	}
+
+	public void deleteFromArray()
+	{
+		int longitud = objetos.Length;
+
+
+		if (verificarCampoInput ()) {
+			int numero = int.Parse (campoInput.textComponent.text);
+			objetos [numero].GetComponent<MeshRenderer> ().material.color = porDefecto;
+		} else {
+			for (int i = longitud - 1; i >= 0; i--) {
+				bool estaVacia = revisarColor (objetos [i].GetComponent<MeshRenderer> ().material.color);
+				if (!estaVacia) {
+					objetos [i].GetComponent<MeshRenderer> ().material.color =	porDefecto;
+					break;
+				}
+			}
+		}
+		
+	}
+
+	void reArrangeArray()
+	{
+		if (objetos [objetos.Length-1].GetComponent<MeshRenderer> ().material.color ==Color.magenta) {
+			Debug.Log ("Entro a Re arrange");
+			GameObject[] temp = new GameObject[(objetos.Length * 3) / 2];
+
+			int tamAnterior = 0;
+
+			for (int i = 0; i < objetos.Length; i++) {
+
+				temp [i] = objetos [i];
+				tamAnterior ++;
+			}
+			objetos = temp;
+			//objetos.Length;
+
+			reCreateCasillas (objetos, tamAnterior-1);
+
+			Debug.Log (objetos.Length+ " parce que");
+			/*
+		for(int i = 0; i<objetos.Length;i++)
+		{
+			Debug.Log ("Entro a agregar nuevo arreglo");
+			temp[i]= objetos[i];
+			}
+		}
+	*/
+		}
+	}
+
+	void reCreateCasillas(GameObject[] z, int y)
+	{
+
+
+		primerObjeto = z [y];
+
+
+		float x = primerObjeto.transform.position.z;
+
+
+
+	
+		for (int i = y+1; i < z.Length; i++) 
+		{
+			
+			z [i] = Instantiate (primerObjeto);
+			Debug.Log (z [i].gameObject.transform.position);
+			z [i].transform.parent = primerObjeto.transform.parent;
+
+			Debug.Log ("pos " + z [i].transform);
+
+			x++;
+			x = x + 0.5f;
+				z [i].transform.position = new Vector3 (0.5f,0,x);
+			z [i].GetComponent<MeshRenderer> ().material.color = Color.white;
+
+		}
+
+		Camera cam = Camera.main;
+		//Debug.Log(z.Length/2);
+		//debugz.Length/2;
+		Vector3 tempPos = cam.transform.position;
+		tempPos.z = z[z.Length/2].transform.position.z;
+		tempPos.x =  z.Length/2 +10f;
+		tempPos.y = tempPos.y + 0.35f;
+		n++;
+
+		cam.transform.position = tempPos;
+
+		//porDefecto = objetos [0].GetComponent<MeshRenderer> ().material.color;
+	}
+
+
+
+}
---
+++ Assets/CreateGraph.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreateGraph : MonoBehaviour {
+
+	public InputField input;
+	public Button boton;
+	string textoInput;
+	float camHeight;
+	float camWidth;
+	Rect rec;
+	public createArk ca;
+
+	GameObject seleccion1;
+	GameObject seleccion2;
+
+	void Awake()
+	{
+		seleccion1 = null;
+		seleccion2 = null;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		verificacionSeleccion ();
+	}
+
+	public void enterNodes()
+	{
+		textoInput = input.GetComponent<InputField> ().text;
+
+		int cantidad = int.Parse (textoInput);
+
+		createNodes (cantidad);
+
+		input.GetComponent<InputField> ().text = "";
+
+		changeState (input.gameObject);
+
+		changeState (boton.gameObject);
+
+	}
+
+	void createNodes(int cantidad)
+	{
+		//getCamHeighWidth ();
+		//Debug.Log ("numero de vertices: " + cantidad); Checked
+
+		for (int i = 0; i < cantidad; i++)
+		{
+//			float randomX = Random.Range (0, camWidth);
+//			float randomY = Random.Range (0, camHeight);
+
+			GameObject nuevo = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			Vector3 temp = new Vector3 (Random.Range(-10.0f,10.0f), Random.Range(-4.2f,6.10f), 0 );
+			nuevo.transform.position = temp;
+			nuevo.GetComponent<MeshRenderer> ().material.color = Color.red;
+			nuevo.tag = "nodo";
+		}
+	}
+
+	void getCamHeighWidth()
+	{
+//		Camera cam = Camera.main;
+//
+//		rec = cam.pixelRect;
+//
+//		camHeight = cam.pixelHeight;
+//		camWidth  =cam.pixelWidth;
+
+	
+
+		//Debug.Log ("numero de  pixels height : " + camHeight); Checked
+		//Debug.Log ("numero de pixel width : " + camWidth);  Checked
+	}
+
+	void changeState(GameObject go)
+	{
+		if (go.activeSelf == true) {
+			go.SetActive (false);
+		} else {
+			go.SetActive (true);
+		}
+	}
+
+	void verificacionSeleccion()
+	{
+		GameObject go = devolverGO ();
+
+		if (go == null) {
+
+		} else {
+			
+			if (go.tag == "nodo") {
+				
+					if (seleccion1 != null) {
+					seleccion2 = go;
+						CreateArc ();
+
+
+				} else if (seleccion1 == null && seleccion2 == null) {
+				
+					seleccion1 = go;
+					cambiarColorActive (seleccion1.GetComponent<MeshRenderer> ());
+				}
+			}
+		}
+	}
+
+
+
+	void CreateArc()
+	{
+		if (seleccion1 == seleccion2) {
+			cambiarColorDisActive (seleccion1.GetComponent<MeshRenderer> ());
+			seleccion1 = null;
+			seleccion2 = null;
+
+		} else {
+			cambiarColorActive (seleccion2.GetComponent<MeshRenderer> ());
+			crearArco( seleccion1 ,  seleccion2);
+		}
+	}
+
+	void crearArco(GameObject go1, GameObject go2)
+	{
+		createArk ca =	new createArk (go1, go2, this);
+
+	}
+
+	GameObject devolverGO()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		//Debug.Log (ray);
+		RaycastHit hit;
+
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.collider.tag == "nodo") {
+					return hit.collider.gameObject;
+
+				} else {
+					return null;
+				}
+			}
+		} else {
+			return null;
+		}
+		return null;
+	}
+
+	void cambiarColorActive(MeshRenderer mr)
+	{
+		mr.material.color = Color.green;
+	}
+	void cambiarColorDisActive(MeshRenderer mr)
+	{
+		mr.material.color = Color.red;
+	}
+
+}
